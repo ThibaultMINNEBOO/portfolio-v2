e126daac298b28ae18bfebe8125c007929b92a40
Thibault MINNEBOO <thibault.minneboo@etudiant.univ-reims.fr>
2023-12-14 15:36:15 +0100

    feat: end crud

---
+++ src/Controller/Admin/ProjectCrudController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Project;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class ProjectCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Project::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name', 'Nom'),
+            TextEditorField::new('description', 'Brève description'),
+            UrlField::new('link', 'Lien vers le projet')->setRequired(false),
+            TextField::new('imageFile')->setFormType(VichImageType::class)->onlyOnForms(),
+            ImageField::new('imageName', 'Image')->setBasePath('/uploads/projects')->onlyOnIndex(),
+        ];
+    }
+}
